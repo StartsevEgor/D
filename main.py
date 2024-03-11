1eae77d58c52db395d20574a30f09158891e3cd9
Yandex_Lyceum_D2 <deloegora@yandex.ru>
2024-03-11 19:43:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from data import db_session
+from data.users import User
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
+
+
+if __name__ == '__main__':
+    main()
