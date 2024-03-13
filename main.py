0063d41c3ca4f09432deef8790aa079f66891b70
Егор Старцев <startseveg@gmail.com>
2024-03-13 21:25:57 +0300

    new change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
@@ -12,6 +13,21 @@ login_manager.init_app(app)
 def main():
     db_session.global_init("db/blogs.db")
     app.run(port=8080, host='127.0.0.1')
+
+
+@app.route("/")
+def index():
+    print(1)
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(2)
+    return render_template("index.html", jobs=jobs)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == '__main__':
