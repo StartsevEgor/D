874d8f3b2bbdd6cb69361f33f5f8aab8320b189c
Егор Старцев <startseveg@gmail.com>
2024-03-14 16:03:40 +0300

    new change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from flask import Flask, render_template, redirect, request, abort
+from flask import Flask, render_template, redirect, request, abort, url_for
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
@@ -20,8 +20,8 @@ def index():
     print(1)
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs).all()
-    print(2)
-    return render_template("index.html", jobs=jobs)
+    print(2, url_for("static", filename='css/style.css'))
+    return render_template('static/templates/index.html', jobs=jobs)
 
 
 @login_manager.user_loader
